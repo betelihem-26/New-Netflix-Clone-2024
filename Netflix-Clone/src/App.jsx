f57b9e5ebba1e225ac83be9e8b756c6e3cafe186
betelihem-26 <bethelhemkiros27@gmail.com>
2024-10-31 03:13:32 -0400

    edit axios

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Home from "./Pages/Home/Home"
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
+import Banner from "./Components/Banner/Banner"
 function App() {
   return (
     <>
     <div className='App'>
-      <Home/>
       <Header/>
+      <Banner/>
       <Footer/>
     </div>
     </>
