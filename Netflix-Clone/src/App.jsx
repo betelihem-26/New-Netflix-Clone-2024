6cb6699aa4ef486423ec1f7289b837c286606ff7
betelihem-26 <bethelhemkiros27@gmail.com>
2024-10-30 23:49:27 -0400

    header and footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from "./Pages/Home/Home"
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div className='App'>
-      <h1>hello </h1>
+      <Home/>
+      <Header/>
+      <Footer/>
     </div>
     </>
   )
